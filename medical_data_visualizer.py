b556f370fa7c62876b767bf10e18816959627a8c
Thomas Ched <tom.chedru@gmail.com>
2024-10-23 15:28:20 +0000

    work started

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,23 +4,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
 df['overweight'] = None
 
 # 3
-
+for field in ['cholesterol','gluc']:
+    df.loc[df[field] == 1, field] = 0
+    df.loc[df[field] > 1, field] = 1
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc','smoke','alco','active','overweight'])
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'variable'],as_index=False).value_counts()
+    print(df_cat)
 
     # 7
 
@@ -31,9 +32,9 @@ def draw_cat_plot():
 
 
     # 9
-    fig.savefig('catplot.png')
-    return fig
-
+    #fig.savefig('catplot.png')
+    #return fig
+    return None
 
 # 10
 def draw_heat_map():
